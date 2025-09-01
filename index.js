4036072fff3a8e3c30be8bcaf4e924caa6953d98
Liz <liz.westapps@gmail.com>
2025-09-01 19:31:40 +0800

    Add POST /api/echo endpoint

--- index.js
+++ index.js
@@ -1,1 +1,2 @@
 const express=require('express'); const cors=require('cors'); const app=express(); app.use(cors()); app.use(express.json()); app.get('/',(req,res)=>res.send('Hello Express!')); app.get('/api/health',(req,res)=>res.json({ ok: true })); app.listen(3000,()=>console.log('Server running on http://localhost:3000'));
+app.post('/api/echo',(req,res)=>res.json({ youSent: req.body }));
