3d3c3395778a6a525896cfc5e4e7c9d2c03b45d8
Liz <liz.westapps@gmail.com>
2025-09-01 19:18:52 +0800

    Add /api/health endpoint

--- index.js
+++ index.js
@@ -1,1 +1,2 @@
 const express = require('express'); const app = express(); app.get('/', (req,res)=>res.send('Hello Express!')); app.listen(3000, ()=>console.log('Server running on http://localhost:3000'));
+app.get('/api/health',(req,res)=>res.json({ ok: true }));
